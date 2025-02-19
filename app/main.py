91decb4fde14cba464235047704a2c53f15f5203
Polina Haltseva <polinayut@gmail.com>
2025-02-19 13:04:32 +0200

    'Final'

--- app/main.py
+++ app/main.py
@@ -45,8 +45,6 @@ class CarWashStation:
         return car.clean_mark
 
     def rate_service(self, rate: int) -> float:
-        self.average_rating = round(
-            (self.average_rating * self.count_of_ratings + rate) / (self.count_of_ratings + 1), 1
-        )
+        self.average_rating = round((self.average_rating * self.count_of_ratings + rate) / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
         return self.average_rating
